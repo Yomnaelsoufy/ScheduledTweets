0cfc7a7ae8543d92465c4f8f48b94f538fbbb1bd
yelsoufy <yelsoufy@ejada.com>
2022-10-30 08:48:20 +0200

    phase 2

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,9 @@
 class MainController < ApplicationController
-    def index
-    end
+     def index
+        if session[:user_id]
+            @user=User.find(session[:user_id])
+        end
+    #     flash[:notice]="Logged in is successfully"
+    #     flash[:alert]="Invalid email or password"
+     end
 end
